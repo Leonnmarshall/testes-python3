34ac038a1a21969768d1d3a974c8265b8c8e1a3b
Leonardo Marçal <66973637+Leonnmarshall@users.noreply.github.com>
2020-07-08 10:56:17 -0300

    Função par ou ímpar
    A função determina se o valor é par ou ímpar.

---
+++ fuc.par.ou.impar.py
@@ -0,0 +1,13 @@
+x = int(input("número"))
+
+def épar(x):
+	return(x%2==0)
+
+def par_ou_ímpar(x):
+	if épar(x): 
+		return "par" 
+	else:
+		return "ímpar" 
+
+print(par_ou_ímpar(4))
+print(par_ou_ímpar(5))
